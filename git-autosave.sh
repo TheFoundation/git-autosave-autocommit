63a2ea31e92b5bd5398580ee138500163b46c353
Any Name <git@b1nch0.us.to>
2019-10-09 01:32:05 +0200

    2019-10-08-23.32

--- git-autosave.sh
+++ git-autosave.sh
@@ -3,11 +3,11 @@
 echo "REMEMBER TO CHANGE INTO A GIT DIRECTORY BEFORE RUNNING THIS SCRIPT"
 
 git_commitpush() { git add -A ;git commit -m "$(date -u +%Y-%m-%d-%H.%M)"" $COMMITCOMMENT" ; git push ; } ; 
-sum=$(find -type f |grep -v ".git" -exec md5sum {} \;|md5sum);
+sum=$(find -type f -exec md5sum {} \;|grep -v ".git" |md5sum);
 
 while (true);do 
   sleep 10;
-  sum_cur=$(find -type f |grep -v ".git" -exec md5sum {} \;|md5sum);
+  sum_cur=$(find -type f -exec md5sum {} \;|grep -v ".git" |md5sum);
   if [ "$sum" == "$sum_cur" ] ; then 
       echo -ne "\rnothing changed@"$(date -u);
   else
